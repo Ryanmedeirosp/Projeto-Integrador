d8bf0d487911c870f7bbec885274051aa78ef35f
ryan <ryan.mporciuncula@gmail.com>
2024-06-14 20:15:21 -0300

    mudanca Ryan

--- JavaScript/scriptInvestimento/script.js
+++ JavaScript/scriptInvestimento/script.js
@@ -23,6 +23,10 @@ function buscarDados(){
            let divAcao = document.createElement('div')
            let imgAcao = document.createElement('img') 
            let nameAcao = document.createElement('p')
+           let precoAcao = document.createElement('p')
+           let rentabilidadeAcao = document.createElement('p')
+           let siglaAcao = document.createElement('p')
+           let setorAcao = document.createElement('p')
            let botao = document.createElement('button')
            botao.textContent = "investir"
            botao.className = "botaoInvestir"
@@ -30,13 +34,26 @@ function buscarDados(){
            divAcao.appendChild(divImgAcao)
            divImgAcao.appendChild(imgAcao)
            divImgAcao.appendChild(nameAcao)
+           divImgAcao.appendChild(siglaAcao)
+           divImgAcao.appendChild(setorAcao)
+           divImgAcao.appendChild(rentabilidadeAcao)
+           divImgAcao.appendChild(precoAcao)
            divImgAcao.appendChild(botao)
            
-           
+
+           nameAcao.className = "nomeAcao"
+           rentabilidadeAcao.className ="nomeAcao"
+           precoAcao.className ="nomeAcao"
+           setorAcao.className ="nomeAcao"
+           siglaAcao.className ="nomeAcao"
            divAcao.className = "divAcao"
            divImgAcao.className = "divImgAcao"
            imgAcao.src = resposta.stocks[index].logo
            nameAcao.textContent = resposta.stocks[index].name
+           rentabilidadeAcao.textContent = resposta.stocks[index].change.toFixed(2) + "%"
+           precoAcao.textContent = "R$"+resposta.stocks[index].close 
+           setorAcao.textContent = resposta.stocks[index].sector
+           siglaAcao.textContent = resposta.stocks[index].stock
            console.log(resposta.stocks[index])
 
         }        
