ec813a6f67efb750cebcc40d1f4e7a7dd1ca17f5
ryan <ryan.mporciuncula@gmail.com>
2024-06-21 20:58:48 -0300

    mudanca

--- JavaScript/scriptCadastro/script.js
+++ JavaScript/scriptCadastro/script.js
@@ -6,16 +6,75 @@ const telefone = document.querySelector("#Box5")
 const senha = document.querySelector("#Box6")
 const confirmarSenha = document.querySelector("#Box7")
 const botao = document.querySelector("#btn")
+const span = document.querySelector("#p1")
+
+confirmarSenha.addEventListener("input",(e)=>{
+    if (confirmarSenha.value !== senha.value) {
+        confirmarSenha.style.border = " 3px solid red"
+        span.textContent = "Suas senhas não estão iguais"
+        senha.style.border = " 3px solid red"
+        
+    }else{
+        confirmarSenha.style.border = " 3px solid white"
+
+        span.textContent = "Ao clicar em Enviar informaçõesnvocê autoriza a RD Investimentos a coletar seus dados pessoais de acordo com a nossa Política de Privacidade com o objetivo de comunicar informações sobre o processo de abertura da sua conta."
+
+        senha.style.border = " 3px solid white"
+        
+    }
+})
+function validarLetras(input) {
+    
+    input.value = input.value.replace(/[^a-zA-Z\s]/g, '');
+}
+nomeCompleto.addEventListener("input",(e)=>{
+    validarLetras(nomeCompleto)
+})
+
+
 
 
 
 botao.addEventListener("click", (event) => {
-    event.preventDefault()
-    if ((nomeCompleto == null) && (cpf == null) && (dataNascimento == null) && (email == null) && (telefone == null) && (senha == null) && (confirmarSenha == null)) {
-        alert("existem campos vázios")
+   
+    if (nomeCompleto.value =="") {
+        nomeCompleto.style.border = " 3px solid red"
+        nomeCompleto.placeholder = "Preencha seu nome"
+
     }
+    if (cpf.value == "") {
+        cpf.style.border = " 3px solid red"
+        cpf.placeholder = "Preencha seu CPF"
+        
+    }
+    if (dataNascimento.value == "") {
+        dataNascimento.style.border = " 3px solid red"
+    }
+    if (email.value == "")  {
+        
+        email.style.border = " 3px solid red"
+        email.placeholder = "Preencha seu email"
+        
+    }
+    if (telefone.value == "") {
+        telefone.style.border = " 3px solid red"
+        telefone.placeholder = "Preencha seu telefone"
+        
+    }
+    if (senha.value == "") {
+        senha.style.border = " 3px solid red"
+        senha.placeholder = "Preencha sua senha"
+        
+    }
+    if (confirmarSenha.value == "") {
+        confirmarSenha.style.border = " 3px solid red"
+        confirmarSenha.placeholder = "Preencha sua senha"
+        
+    }
+    
+
     if (cpf.value.length < 11) {
-        alert("cpf incorreto")
+        // alert("cpf incorreto")
     }
     console.log(validaCPF(cpf.value))
 });
@@ -68,12 +127,12 @@ const validaCPF = (cpf) => {
     let cpfCorreto = cpf.substring(0, 9) + primeiroDigitoVerificador + segundoDigitoVerificador
 
     if(cpf !== cpfCorreto) {
-        return false
         console.log("CPF inválido")
+        return false
+        
     }
 
     console.log("CPF Válido")
     return true
 
 }
-
