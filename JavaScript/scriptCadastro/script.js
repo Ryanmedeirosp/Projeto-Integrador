5f9615fe22f72ee34a2d663d6526c29d54045c7e
José Bulhões <josebgojunior@gmail.com>
2024-06-21 16:28:09 -0300

    mudanca junior

--- JavaScript/scriptCadastro/script.js
+++ JavaScript/scriptCadastro/script.js
@@ -1,31 +1,78 @@
-const nomeCompleto = document.querySelector("#box1")
+const nomeCompleto = document.querySelector("#Box1")
 const cpf = document.querySelector("#Box2")
-const dataNascimento = document.querySelector("#box3")
-const email = document.querySelector("#box4")
-const telefone = document.querySelector("#box5")
-const senha = document.querySelector("#box6")
-const confirmarSenha = document.querySelector("#box7")
+const dataNascimento = document.querySelector("#Box3")
+const email = document.querySelector("#Box4")
+const telefone = document.querySelector("#Box5")
+const senha = document.querySelector("#Box6")
+const confirmarSenha = document.querySelector("#Box7")
 const botao = document.querySelector("#btn")
 
 
 
 botao.addEventListener("click", (event) => {
     event.preventDefault()
-    if ((nomeCompleto === null) && (cpf === null) && (dataNascimento === null) && (email === null) && (telefone === null) && (senha === null) && (confirmarSenha === null)) {
+    if ((nomeCompleto == null) && (cpf == null) && (dataNascimento == null) && (email == null) && (telefone == null) && (senha == null) && (confirmarSenha == null)) {
         alert("existem campos vázios")
     }
-
     if (cpf.value.length < 11) {
         alert("cpf incorreto")
     }
 
-    
-
 });
 
-cpf.addEventListener("input", (event) => {
-    
-    if(cpf.value.length == 12) {
-        cpf.value = cpf.value.slice(0, -1)
+cpf.addEventListener("keypress", () => {
+    let inputLength = cpf.value.length
+
+    if (inputLength === 3 || inputLength === 7) {
+        cpf.value += "."
+    }else if(inputLength === 11) {
+        cpf.value += "-"
     }
 });
+
+const validaCPF = (cpf) => {
+
+    cpf = cpf.replace(/\D/g, "")
+
+    console.log
+
+    if(cpf.length !== 11){
+        console.clear()
+        console.error("CPF inválido.")
+        return
+    }
+
+    const proximoDigitoVerificador = (cpfIncompleto) => {
+
+        let somatoria = 0
+
+        for (let index = 0; index < cpfIncompleto.length; index++) {
+
+            let digitoAtual = cpfIncompleto.charAt(index)
+
+            let constante = (cpfIncompleto.length + 1 - index)
+
+            somatoria += Number(digitoAtual) * constante
+
+            console.log(somatoria)
+        }
+
+        const resto = somatoria % 11
+        return resto < 2 ? "0" : (11 - resto).toString()
+    }
+
+    let primeiroDigitoVerificador = proximoDigitoVerificador(cpf.substring(0, 9))
+    let segundoDigitoVerificador = proximoDigitoVerificador(cpf.substring(0, 9) + primeiroDigitoVerificador)
+    
+    let cpfCorreto = cpf.substring(0, 9) + primeiroDigitoVerificador + segundoDigitoVerificador
+
+    if(cpf !== cpfCorreto) {
+        alert("cpf inválido")
+    }
+
+    console.log("CPF Válido")
+    return true
+
+}
+
+validaCPF("131.330.824-26")
