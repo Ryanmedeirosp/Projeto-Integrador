de19f0382d919ba82205a04538b6582ad20b8b51
José Bulhões <josebgojunior@gmail.com>
2024-06-25 20:45:11 -0300

    mudanca junior

--- JavaScript/scriptCadastro/script.js
+++ JavaScript/scriptCadastro/script.js
@@ -55,101 +55,104 @@ confirmarSenha.addEventListener("input",(e)=>{
         
     }
 })
+
+
 function validarLetras(input) {
-    
     input.value = input.value.replace(/[^a-zA-Z\s]/g, '');
 }
+
 nomeCompleto.addEventListener("input",(e)=>{
     validarLetras(nomeCompleto)
 })
-
-
-
-
 
 botao.addEventListener("click", (event) => {
    
     if (nomeCompleto.value =="") {
         nomeCompleto.style.border = " 3px solid red"
         nomeCompleto.placeholder = "Preencha seu nome"
-
     }
+
     if (cpf.value == "") {
         cpf.style.border = " 3px solid red"
         cpf.placeholder = "Preencha seu CPF"
         
     }
+
     if (dataNascimento.value == "") {
         dataNascimento.style.border = " 3px solid red"
     }
-    if (email.value == "")  {
-        
+
+    if (email.value == "")  {    
         email.style.border = " 3px solid red"
         email.placeholder = "Preencha seu email"
         
     }
+
     if (telefone.value == "") {
         telefone.style.border = " 3px solid red"
-        telefone.placeholder = "Preencha seu telefone"
-        
+        telefone.placeholder = "Preencha seu telefone"     
     }
+
     if (senha.value == "") {
         senha.style.border = " 3px solid red"
         senha.placeholder = "Preencha sua senha"
         
     }
+
     if (confirmarSenha.value == "") {
         confirmarSenha.style.border = " 3px solid red"
         confirmarSenha.placeholder = "Preencha sua senha"
-        
     }
     
-
     if (cpf.value.length < 11) {
         console.log("campo CPF não preenchido corretamente.")
     }
+
+    const senhaValue = senha.value;
+    const confirmarSenhaValue = confirmarSenha.value;
+
+    const senhaRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+    if (!senhaRegex.test(senhaValue)) {
+        console.log('A senha deve ter no mínimo 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caracter especial.')
+    }else if (senhaValue !== confirmarSenhaValue) {
+        console.log('As senhas não correspondem.');
+    } else{
+        console.log('Senha válida!');
+    }
+
     console.log(validaCPF(cpf.value))
 });
 
 cpf.addEventListener("input", (event) => {
-
     let currentValue = cpf.value;
+    let formattedValue = currentValue.replace(/[^0-9]/g, '');
 
-    currentValue = currentValue.replace(/[^0-9.\\-]/g, '');
-
-    cpf.value = currentValue;
-
-    let ultimoChar = currentValue.slice(-1);
-
-    if(event.inputType === "deleteContentBackward") {
-
-        cpf.value = currentValue.slice(0, -1);
-
-    } else {
-        
-        let inputLength = currentValue.length;
-
-        if (inputLength === 3 || inputLength === 7) {
-
-            cpf.value += ".";
-
-        } else if (inputLength === 11) {
-
-            cpf.value += "-";
-
-        }
+    if (event.inputType === "deleteContentBackward") {
+        formattedValue = formattedValue.slice(0, -1);
     }
+
+    let newValue = '';
+    for (let i = 0; i < formattedValue.length; i++) {
+        if (i === 3 || i === 6) {
+            newValue += '.';
+        }
+        if (i === 9) {
+            newValue += '-';
+        }
+        newValue += formattedValue[i];
+    }
+
+    cpf.value = newValue;
 });
 
 const validaCPF = (cpf) => {
 
     cpf = cpf.replace(/\D/g, "")
 
-    console.log
-
     if(cpf.length !== 11){
-        console.clear()
-        console.error("Campo cpf não foi preenchido corretamente.")
+        console.error("Campo CPF não foi preenchido corretamente.")
         return
     }
 
